bd0da0a121e919c91c18c7f9736fa43a5ac4496a
Jeric Devaras <jericjdevaras@outlook.com>
2026-02-17 23:04:02 +0800

    kickstarter

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ window._globals = {
 window.addEventListener("hashchange", updateUI);
 
 // creates a readable project name from the (technical) GitHub repository name
-function readableRepoName (sName) {
+function readableRepoName(sName) {
 	// split by - by default
 	let aWords = sName.split("-");
 	// try splitting with _ instead
@@ -26,18 +26,18 @@ function readableRepoName (sName) {
 }
 
 // creates all items to be displayed
-function createContent (aItems) {
+function createContent(aItems) {
 	// extract display mode
 	let oURL = new URL("https://dummy.com");
 	oURL.search = window.location.hash.substring(1);
 	const sDisplay = oURL.searchParams.get("display") || "card";
 	// generate and show HTML
-	const sResult = aItems.map((oItem) => generateItem(sDisplay, oItem)).join ("");
+	const sResult = aItems.map((oItem) => generateItem(sDisplay, oItem)).join("");
 	updateContent(sDisplay, sResult, aItems);
 }
 
 // updates the content area
-function updateContent (sDisplay, sResult, aItems) {
+function updateContent(sDisplay, sResult, aItems) {
 	// flush html
 	window.document.getElementById((sDisplay === "card" ? "card" : "row") + "s").innerHTML = sResult;
 
@@ -52,7 +52,7 @@ function updateContent (sDisplay, sResult, aItems) {
 }
 
 // updates UI state based on Hash
-function updateUI () {
+function updateUI() {
 	if (window._globals.ignoreNextHashChange) {
 		return;
 	}
@@ -70,7 +70,7 @@ function updateUI () {
 }
 
 // updates hash based on UI state (note: does not work on IE11, needs URL Polyfill)
-function updateHash (sKey, sValue) {
+function updateHash(sKey, sValue) {
 	let oURL = new URL("https://dummy.com");
 	oURL.search = window.location.hash.substring(1);
 	sValue ? oURL.searchParams.set(sKey, sValue) : oURL.searchParams.delete(sKey);
@@ -83,7 +83,7 @@ function updateHash (sKey, sValue) {
 }
 
 // replace broken images with a default image
-function registerFallbackImage (oNode) {
+function registerFallbackImage(oNode) {
 	let aImages = oNode.getElementsByTagName("img");
 	for (let i = 0; i < aImages.length; i++) {
 		aImages[i].addEventListener("error", function () {
@@ -95,9 +95,9 @@ function registerFallbackImage (oNode) {
 }
 
 // helper function to display each language in a different static color
-function stringToColor (sString) {
+function stringToColor(sString) {
 	Math.seedrandom(sString);
-	const rand = Math.random() * Math.pow(255,3);
+	const rand = Math.random() * Math.pow(255, 3);
 	Math.seedrandom();
 
 	let sColor = "#";
@@ -106,20 +106,20 @@ function stringToColor (sString) {
 }
 
 // fetches an image for the detected programming language
-function getRepoLanguage (sLanguage) {
+function getRepoLanguage(sLanguage) {
 	let sLanguageShort = "N/A";
 	let sFontSize;
 
 	if (sLanguage) {
 		sLanguageShort = sLanguage;
-		if(sLanguageShort.length > 4) {
+		if (sLanguageShort.length > 4) {
 			// smart length reduction
 			if (sLanguageShort.match(/[A-Z][a-z]+/g) && sLanguageShort.match(/[A-Z][a-z]+/g).length > 1) {
 				// abbreviate by capital letters and cut off at 4 letters
-				sLanguageShort = sLanguageShort.match(/[A-Z][a-z]+/g).reduce((x, y) => x.substr(0,1) + y.substr(0, 1));
+				sLanguageShort = sLanguageShort.match(/[A-Z][a-z]+/g).reduce((x, y) => x.substr(0, 1) + y.substr(0, 1));
 			} else if (sLanguageShort.match(/[auoie]+/g)) {
 				// remove vowels
-				while(sLanguageShort.match(/[auoie]+/g) && sLanguageShort.length > 4) {
+				while (sLanguageShort.match(/[auoie]+/g) && sLanguageShort.length > 4) {
 					sLanguageShort = sLanguageShort.replace(/[auoie]{1}/, "");
 				}
 			}
@@ -139,7 +139,7 @@ function getRepoLanguage (sLanguage) {
 
 	// a pseudo-random color coding and the shortened text
 	return window._globals.templates.language({
-		color: (sLanguageShort !== "N/A" ?  stringToColor(sLanguage) : ""),
+		color: (sLanguageShort !== "N/A" ? stringToColor(sLanguage) : ""),
 		fontSize: sFontSize,
 		language: sLanguage,
 		languageShort: sLanguageShort
@@ -149,7 +149,7 @@ function getRepoLanguage (sLanguage) {
 // get a visual representation of the Activity Score of the repo.
 // - if Activity Score exists: an image representing how active the repo is
 // - if Activity Score doesn't exist: a placeholder
-function getRepoActivity (oRepo) {
+function getRepoActivity(oRepo) {
 	let sScoreIndicator = "<div class=\"tooltipped score\" data-position=\"top\" data-tooltip=\"Activity: not available\">N/A</div>",
 		vScoreNumeric = "N/A";
 
@@ -162,7 +162,7 @@ function getRepoActivity (oRepo) {
 }
 
 // fetches the corresponding image for the activity score
-function getActivityLogo (iScore) {
+function getActivityLogo(iScore) {
 	let sLogo = "images/activity/0.png",
 		sActivityLevel = "None";
 
@@ -193,7 +193,7 @@ function getActivityLogo (iScore) {
 }
 
 // calculations a color for the participation value
-function getParticipationColor (iValue) {
+function getParticipationColor(iValue) {
 	let iOpacity;
 	if (!iValue) {
 		return false;
@@ -201,14 +201,14 @@ function getParticipationColor (iValue) {
 	if (iValue === 0) {
 		iOpacity = 0;
 	} else {
-		iOpacity = Math.log(iValue)/4 + 0.03; // 50 = 1, scale logarithmically below
+		iOpacity = Math.log(iValue) / 4 + 0.03; // 50 = 1, scale logarithmically below
 	}
 	iOpacity = Math.min(1, iOpacity);
 	return "rgba(50, 205, 50, " + iOpacity + ")";
 }
 
 // creates an HTMl-based heatmap for the current week and the previous 12 weeks from participation stats
-function createParticipationChart (oRepo) {
+function createParticipationChart(oRepo) {
 	let aParticipation = oRepo._InnerSourceMetadata && oRepo._InnerSourceMetadata.participation;
 	if (!aParticipation) {
 		return "N/A";
@@ -268,7 +268,7 @@ function createParticipationChart (oRepo) {
 }
 
 // creates HTML for and displays a project details modal
-function showModal (vRepoId, oEvent) {
+function showModal(vRepoId, oEvent) {
 	// don't open modal when clicking on direct links
 	if (oEvent && oEvent.target.href) {
 		return;
@@ -309,8 +309,10 @@ function showModal (vRepoId, oEvent) {
 			? `${oRepo.html_url}/blob/${oRepo.default_branch}/${oRepo._InnerSourceMetadata.guidelines}`
 			: oRepo.html_url;
 
+	let stemplateUrl = `https://github.com/new?owner=${oRepo.owner.login}&template_name=${oRepo.name}&template_owner=${oRepo.owner.login}`;
+
 	let oContext = {
-		"id" : (typeof oRepo.id === "string" ? "'" + oRepo.id + "'" : oRepo.id),
+		"id": (typeof oRepo.id === "string" ? "'" + oRepo.id + "'" : oRepo.id),
 		"mediaURL": sLogoURL,
 		"title": sTitle,
 		"repoURL": oRepo.html_url,
@@ -318,6 +320,9 @@ function showModal (vRepoId, oEvent) {
 		"description": sDescription,
 		"topics": oRepo._InnerSourceMetadata && oRepo._InnerSourceMetadata.topics,
 		"stars": oRepo.stargazers_count,
+		"templateClass": (oRepo.is_template == true) ? "template" : "no-template",
+		"isTemplate": oRepo.is_template,
+		"templateUrl": stemplateUrl,
 		"issues": oRepo.open_issues_count,
 		"forks": oRepo.forks_count,
 		"score": sScoreIndicator,
@@ -354,7 +359,7 @@ function showModal (vRepoId, oEvent) {
 }
 
 // fills the HTML template for a project item
-function generateItem (sDisplay, oRepo) {
+function generateItem(sDisplay, oRepo) {
 	let sLogoURL = oRepo._InnerSourceMetadata && oRepo._InnerSourceMetadata.logo
 		? oRepo._InnerSourceMetadata.logo.startsWith("http") || oRepo._InnerSourceMetadata.logo.startsWith("./")
 			? oRepo._InnerSourceMetadata.logo
@@ -377,14 +382,19 @@ function generateItem (sDisplay, oRepo) {
 			? `${oRepo.html_url}/blob/${oRepo.default_branch}/${oRepo._InnerSourceMetadata.guidelines}`
 			: oRepo.html_url;
 
+	let stemplateUrl = `https://github.com/new?owner=${oRepo.owner.login}&template_name=${oRepo.name}&template_owner=${oRepo.owner.login}`;
+
 	let oContext = {
-		"id" : (typeof oRepo.id === "string" ? "'" + oRepo.id + "'" : oRepo.id),
+		"id": (typeof oRepo.id === "string" ? "'" + oRepo.id + "'" : oRepo.id),
 		"mediaURL": sLogoURL,
 		"title": sTitle,
 		"repoURL": oRepo.html_url,
 		"repoTitle": oRepo.owner.login + "/" + oRepo.name,
 		"description": sDescription,
 		"stars": oRepo.stargazers_count,
+		"templateClass": (oRepo.is_template == true) ? "template" : "no-template",
+		"isTemplate": oRepo.is_template,
+		"templateUrl": stemplateUrl,
 		"issues": oRepo.open_issues_count,
 		"forks": oRepo.forks_count,
 		"score": getRepoActivity(oRepo)[0],
@@ -397,7 +407,7 @@ function generateItem (sDisplay, oRepo) {
 }
 
 // load repos.json file and display the list of projects from it
-window.document.addEventListener("DOMContentLoaded", function() {
+window.document.addEventListener("DOMContentLoaded", function () {
 	// load data
 	let oXHR = new XMLHttpRequest();
 	oXHR.open("GET", "repos.json");
@@ -438,7 +448,7 @@ window.document.addEventListener("DOMContentLoaded", function() {
 });
 
 // fill language filter list based on detected languages
-function fillLanguageFilter () {
+function fillLanguageFilter() {
 	let aAllLanguages = [];
 	// create a unique list of languages
 	window._globals.allRepos.map(repo => {
@@ -471,7 +481,7 @@ function addLanguageIconsToFilter() {
 }
 
 // sort the cards by chosen parameter (additive, combines filter or search)
-function sort (sParam) {
+function sort(sParam) {
 	let aResult;
 	if (["name", "full_name"].includes(sParam)) {
 		// sort alphabetically
@@ -500,7 +510,7 @@ function sort (sParam) {
 }
 
 // filter the cards by chosen parameter (resets search)
-function filter (sParam) {
+function filter(sParam) {
 	let aResult;
 	if (sParam !== "All") {
 		if (sParam === "N/A") { // other
@@ -575,7 +585,7 @@ function search(sParam) {
 }
 
 // toggles the display between card and table view
-function display (sParam) {
+function display(sParam) {
 	// update UI
 	window.document.getElementById("display").checked = (sParam !== "list");
 	// toggle active icon
@@ -592,134 +602,4 @@ function display (sParam) {
 	window.document.getElementById(sParam !== "list" ? "rows" : "cards").innerHTML = "";
 	window.document.getElementById(sParam !== "list" ? "cards" : "list").style.display = "block";
 	window.document.getElementById(sParam !== "list" ? "list" : "cards").style.setProperty("display", "none", "important");
-}
-
-async function fetch_repos(url) {
-	let response = await fetch(url);
-	let data = await response.json();
-	return data;
-}
-
-function generate_schema(data) {
-	function collect_unique_keys(obj, path = "") {
-		/*
-		* recursevily collects all unique keys
-		* this is necessary for converting to csv file
-		*/
-		for (const key in obj) {
-			const newPath = path ? `${path}.${key}` : key;
-			if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
-				collect_unique_keys(obj[key], newPath);
-			} else {
-				allKeys[newPath] = Array.isArray(obj[key]) ? ["hs"] : "hs"; // hiee
-			}
-		}
-	}
-
-	function key_finalizer(obj, path) {
-		/*
-		* This function creates the template json object
-		* it splits the path by '.' and checks if the corresponding key exists
-		* if not, it creates a new object
-		* at the end it returns the full template object
-		*/
-
-		path.split('.').reduce((acc, part, index, arr) => {
-			if (!acc[part]) {
-				acc[part] = index === arr.length - 1 ? obj[path] : {};
-			}
-			return acc[part];
-		}, template);
-	}
-
-	let allKeys = {};
-	let template = {};
-
-	data.forEach(item => collect_unique_keys(item));
-	Object.keys(allKeys).forEach(keyPath => key_finalizer(allKeys, keyPath));
-	return template;
-}
-
-async function repos_to_csv() {
-	/*
-	* this is the main method of csv download
-	* first it fetches the data, followed by generating the template of the json
-	*
-	* then it flattens the json object and extracts the keys
-	* and subsequently flattens the each entry of the json object
-	* for any entry, where there is no data on keys which exist in the template, it will be set to "nodata"
-	* at the end, it merges all and calls the download_csv_file function
-	*/
-	let data = await fetch_repos(`${window.location.origin}/repos.json`);
-	let template = generate_schema(data);
-
-	let csv_keys = Object.keys(flatten(template));
-	let csv_rows = [csv_keys.join(";")];
-
-	data.forEach(entry => {
-		let flattenedEntry = flatten(entry);
-		let row = csv_keys.map(key => (flattenedEntry[key] !== undefined ? flattenedEntry[key] : "nodata"));
-		csv_rows.push(row.join(";"));
-	});
-
-	let csv_content = csv_rows.join("\n");
-	download_csv_file(csv_content, "repos.csv");
-}
-
-
-const flatten = (object, path = '') =>
-	Object.entries(object).reduce((acc, [key, val]) => {
-		/*
-		* this function flattens the json object, which will be used to create the csv headers
-		* step by step, works like this
-		* if value is undefined -> return accumulator
-		* if path already exsits, append the current key to the current path, thus basically creating a nest .dot notation
-		* if the value is an array, don't seperate the value and flatten each one by itself, rather save array as one value
-		* if the value is an object (not date or regex) it checks if there are custom functions, if yes use that value, otherwise merge into accumulator
-		* if the value are strings / integers, directly assign and just return them
-		* source: https://gist.github.com/penguinboy/762197?permalink_comment_id=4058844   -> array unification was added
-		*/
-		if (val === undefined) return acc;
-		if (path) key = `${path}.${key}`;
-		if (Array.isArray(val)) {
-			acc[key] = `[${val.join(",")}]`;
-		} else if (typeof val === 'object' && val !== null && !(val instanceof Date) && !(val instanceof RegExp)) {
-			if (val !== val.valueOf()) {
-				return { ...acc, [key]: val.valueOf() };
-			}
-			return { ...acc, ...flatten(val, key) };
-		} else {
-			acc[key] = val;
-		}
-		return acc;
-	}, {});
-
-
-function description_formatting(entry) {
-	/*
-	* should there be the rare case where the semicolon char ';' appears in the description,
-	* then this function needs to be called, so that the seperation by the delimiter works
-	* use this in a for-loop
-	*/
-	let description = entry.description;
-	description = description.replace(';', ',');
-	return description;
-}
-
-function download_csv_file(csv, file_name) {
-	/*
-	* This function creates the downloadable object in the browser
-	* and triggers the download automatically once run
-	*/
-	let blob = new Blob([csv], { type: 'text/csv' });
-	let link = document.createElement('a');
-	if (link.download !== undefined) {
-		let url = URL.createObjectURL(blob);
-		link.setAttribute('href', url);
-		link.setAttribute('download', file_name);
-		link.style.visibility = 'hidden';
-		document.body.appendChild(link);
-		link.click();
-		document.body.removeChild(link);
-	}
 }
